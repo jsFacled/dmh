f739c8efb8cd89efc97e3f76af1a2246d149d9c8
faGitLab <facled@yahoo.com.ar>
2024-07-18 09:52:45 -0300

    msvc-users init

---
+++ msvc-users/src/test/java/com/DigitalMoneyHouse/msvc_users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalMoneyHouse.msvc_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
