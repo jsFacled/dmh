a6ec8c56208a8fe273c3d3b616210d83cf8d8fa6
jsFacled <facled@gmail.com>
2024-07-21 15:27:05 -0300

    creando msvc-accounts

---
+++ msvc-account/src/main/java/com/DigitalMoneyHouse/msvc_accounts/MsvcAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalMoneyHouse.msvc_accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAccountsApplication.class, args);
+	}
+
+}
