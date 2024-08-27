e6dfce0f7cf9b0b2c4da84874c71dc7591e6cd2d
faGitLab <facled@yahoo.com.ar>
2024-08-27 16:55:26 -0300

    ms-configServer iniciado

---
+++ msvc-configServer/src/main/java/com/DigitalMoneyHouse/msvc_configServer/MsvcConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalMoneyHouse.msvc_configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcConfigServerApplication.class, args);
+	}
+
+}
