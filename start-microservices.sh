3a05cc5d3c0753b10c7db7e8d53fdfd227277ee2
faGitLab <facled@yahoo.com.ar>
2024-09-13 20:03:27 -0300

    modificando uaM en applicaation.yml

--- start-microservices.sh
+++ start-microservices.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Inicia el microservicio msvc-configServer
-mintty bash -c "cd msvc-configServer && ./mvnw spring-boot:run; exec bash"
+mintty bash -c "cd msvc-configServer && ./mvnw spring-boot:run"
 
 # Espera 10 segundos para asegurarse de que msvc-configServer esté completamente en funcionamiento
 sleep 10
 
 # Inicia el microservicio msvc-eureka
-mintty bash -c "cd msvc-eureka && ./mvnw spring-boot:run; exec bash"
+mintty bash -c "cd msvc-eureka && ./mvnw spring-boot:run"
 
 # Espera 10 segundos para asegurarse de que msvc-eureka esté completamente en funcionamiento
 sleep 10
 
 # Inicia los otros microservicios
-mintty bash -c "cd msvc-accpimts && ./mvnw spring-boot:run; exec bash"
-mintty bash -c "cd msvc-users && ./mvnw spring-boot:run; exec bash"
-mintty bash -c "cd msvc-cards && ./mvnw spring-boot:run; exec bash"
-mintty bash -c "cd msvc-transactions && ./mvnw spring-boot:run; exec bash"
-mintty bash -c "cd msvc-usersAccountsManager && ./mvnw spring-boot:run; exec bash"
-mintty bash -c "cd msvc-gateway && ./mvnw spring-boot:run; exec bash"
+mintty bash -c "cd msvc-accpimts && ./mvnw spring-boot:run"
+mintty bash -c "cd msvc-users && ./mvnw spring-boot:run"
+mintty bash -c "cd msvc-cards && ./mvnw spring-boot:run"
+mintty bash -c "cd msvc-transactions && ./mvnw spring-boot:run"
+mintty bash -c "cd msvc-usersAccountsManager && ./mvnw spring-boot:run"
+mintty bash -c "cd msvc-gateway && ./mvnw spring-boot:run"
 
 echo "Todos los microservicios están en ejecución."
 
