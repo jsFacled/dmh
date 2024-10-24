2daf9c299d57c909f09d51a3414865d654cc7538
faGitLab <facled@yahoo.com.ar>
2024-10-24 18:05:59 -0300

    En accounts el generador de Alias debe tomar el archivo word.txt desde inputsetream para que docker pueda llegar al mismo

--- msvc-accounts/src/main/java/com/DigitalMoneyHouse/msvc_accounts/service/AliasGeneratorService.java
+++ msvc-accounts/src/main/java/com/DigitalMoneyHouse/msvc_accounts/service/AliasGeneratorService.java
@@ -2,11 +2,16 @@ package com.DigitalMoneyHouse.msvc_accounts.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 public class AliasGeneratorService {
@@ -14,9 +19,29 @@ public class AliasGeneratorService {
     private List<String> words;
     private Random random = new Random();
 
+
+    /*
+     * Esta forma en Docker no funciona porque al leer words.txt utiliza otra ruta 
+     */
+    /*
     public AliasGeneratorService() {
         try {
             words = Files.readAllLines(Paths.get("src/main/resources/static/words.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read words file", e);
+        }
+    }
+*/
+
+//Forma sugerida para Dockerizar
+public AliasGeneratorService() {
+        try (InputStream inputStream = getClass().getResourceAsStream("/static/words.txt")) {
+            if (inputStream == null) {
+                throw new RuntimeException("Could not find words file");
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                words = reader.lines().collect(Collectors.toList());
+            }
         } catch (IOException e) {
             throw new RuntimeException("Could not read words file", e);
         }
