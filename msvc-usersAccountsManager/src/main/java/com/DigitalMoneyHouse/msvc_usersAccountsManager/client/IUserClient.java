162b2e3524fba4ac413f75456df8b485bf6f06b1
faGitLab <facled@yahoo.com.ar>
2024-10-27 08:45:16 -0300

    cambiando comunicacion con feign client, se elimina url localhost para que quede solamente el nombre del servicio registrado en eureka

--- msvc-usersAccountsManager/src/main/java/com/DigitalMoneyHouse/msvc_usersAccountsManager/client/IUserClient.java
+++ msvc-usersAccountsManager/src/main/java/com/DigitalMoneyHouse/msvc_usersAccountsManager/client/IUserClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-@FeignClient(name = "msvc-users", url = "http://localhost:8082/users")
+@FeignClient(name = "msvc-users")
 public interface IUserClient {
 
     @PostMapping("/create")
